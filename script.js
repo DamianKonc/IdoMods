0d2c26833324ff956eaeb39236e2022b41e54bbb
Damiankonc <damian.koncewicz@wp.pl>
2025-03-05 15:03:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// This script runs as soon as the page loads
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The page has loaded successfully!");
+});
+
+function sayHello() {
+    alert("Hello! The JavaScript is working.");
+}
